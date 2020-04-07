d3dbe5ac0aa3d3c0ded9e9e2ef8a4668971f8b9d
naghirneac <nag.anya@mail.ru>
2020-04-07 23:59:19 +0300

    Added TSingletone, Registry, BlogApp + config settings

---
+++ app/SBlog/Core/BlogApp.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\SBlog\Core;
+
+//citirea parametrilor
+class BlogApp
+{
+    public static $app;
+
+    public static function getInstance()
+    {
+        self::$app = Registry::instance();
+        self::getParams();
+        return self::$app;
+    }
+
+    protected static function getParams()
+    {
+        $params = require CONF . '/params.php';
+
+        if (!empty($params)){
+            foreach ($params as $k => $v){
+                self::$app->setProperty($k, $v);
+            }
+        }
+    }
+}
