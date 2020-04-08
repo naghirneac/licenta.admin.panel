868e998276426908566cb830aa778e5c217fe39b
naghirneac <nag.anya@mail.ru>
2020-04-08 20:13:53 +0300

    Added routes for admins

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,33 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
+    */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Auth::routes();
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
+    /*Admin side*/
+    Route::group(['middleware' => ['status', 'auth']], function (){
+        $groupData = [
+            'namespace' => 'Blog\Admin',
+            'prefix' => 'admin',
+        ];
+
+        Route::group($groupData, function (){
+            Route::resource('index', 'MainController')
+                ->names('blog.admin.index');
+        });
+    });
